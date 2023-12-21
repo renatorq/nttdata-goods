66dd30d84384167a527a45c30a0f1272efd3eab0
Pedro Rios <prios@canvia.com>
2023-12-21 18:32:08 -0500

    primer commit

---
+++ src/main/java/com/nttdata/goods/NttdataGoodsApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.goods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NttdataGoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NttdataGoodsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/goods/NttdataGoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.goods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttdataGoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
